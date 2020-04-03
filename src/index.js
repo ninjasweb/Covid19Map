573ebfc997b56d58dfdf6105e9e65e82337017ef
NinjasWeb <hola@ninjasweb.com>
2020-04-03 16:08:03 -0500

    Borrar Comentarios

--- src/index.js
+++ src/index.js
@@ -64,23 +64,6 @@ class CovidTest extends Component {
 }
 
 
-	//function showError(error) {
-	//	switch(error.code) {
-	//	  case error.PERMISSION_DENIED:
-	//		alert("User denied the request for Geolocation.");
-	//		break;
-	//	  case error.POSITION_UNAVAILABLE:
-	//		"Location information is unavailable."
-	//		break;
-	//	  case error.TIMEOUT:
-	//		alert("The request to get user location timed out.");
-	//		break;
-	//	  case error.UNKNOWN_ERROR:
-	//		alert("An unknown error occurred.");
-	//		break;
-	//	}
-	// } 
-
 
 		
 	//Obtener Coordenadas
