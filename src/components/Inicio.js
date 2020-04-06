292ca5079fd0baf61f1bb1410245199803355e6d
NinjasWeb <hola@ninjasweb.com>
2020-04-05 23:23:44 -0500

    Background Image
    Agregamos la imagen de fondo en el inicio de la App.

--- src/components/Inicio.js
+++ src/components/Inicio.js
@@ -3,6 +3,7 @@ import { Button } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 
 
+
 const useStyles = makeStyles({
     root: {
       background: 'rgb(70,113,198)',
@@ -38,12 +39,15 @@ const useStyles = makeStyles({
 const Inicio = ({showshow}) => {
     const classes = useStyles();
     return(
-        <div className="inicio" >
+      <div className="bg">
+        <div className="inicio">
             <h1 className="titulo1">TEST DE AUTOEVALUACIÓN <br></br> DE SÍNTOMAS DEL COVID-19</h1>
             <h3 className="subtitulo">Un proyecto de código abierto<br></br> para ayudar a contener y localizar posibles brote del virus</h3>
             <Button className={classes.root} onClick={showshow}>EMPEZAR TEST</Button>
             <p className="copydata">Por favor <b>permite</b> que esta app acceda a tu ubicación. <br></br> no requerimos ningún otro dato personal más.</p>
         </div>
+      </div>
+
 
     );
 };
