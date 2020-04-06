bff49f03c406e3919e2db4159bae8ce35c15289b
NinjasWeb <hola@ninjasweb.com>
2020-04-05 23:46:17 -0500

    Repositorio Github

--- src/components/Inicio.js
+++ src/components/Inicio.js
@@ -9,7 +9,7 @@ const useStyles = makeStyles({
       background: 'rgb(70,113,198)',
       border: 0,
       borderRadius: 3,
-      boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+      boxShadow: '0px 2px 10px rgba(4, 3, 5, 0.8)',
       color: 'white',
       height: 48,
       padding: '0 30px',
@@ -34,10 +34,9 @@ const useStyles = makeStyles({
   });
 
 
-
-
 const Inicio = ({showshow}) => {
     const classes = useStyles();
+    const img1 = require(`../img/github.png`);
     return(
       <div className="bg">
         <div className="inicio">
@@ -45,6 +44,7 @@ const Inicio = ({showshow}) => {
             <h3 className="subtitulo">Un proyecto de código abierto<br></br> para ayudar a contener y localizar posibles brote del virus</h3>
             <Button className={classes.root} onClick={showshow}>EMPEZAR TEST</Button>
             <p className="copydata">Por favor <b>permite</b> que esta app acceda a tu ubicación. <br></br> no requerimos ningún otro dato personal más.</p>
+            <a href="https://www.github.com/ninjasweb/Covid19Map" target="_blank"><img src={img1} alt="github"/></a>
         </div>
       </div>
 
