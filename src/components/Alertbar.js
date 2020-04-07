5b2e5e1c376b0e5c60f878e8db5c88574bd32a6c
NinjasWeb <hola@ninjasweb.com>
2020-04-07 00:53:21 -0500

    Ajustando Página de Resultados

--- src/components/Alertbar.js
+++ src/components/Alertbar.js
@@ -5,18 +5,27 @@ function Advertencia(score) {
     if (score > 30){
       return (
         <Fragment>
+        <div className="mensaje">
+        <p>Tus respuestas sugieren la posibilidad de tener sintomatología<br></br> compatible con COVID-19</p>
         <input type="radio" className="radio" name="progress" value="twentyfive" id="twentyfive" defaultChecked></input>
         <div className="progress">
             <div className="progress-bar"></div>
+        </div>
+          <ul>
+            <li>Observa tu evolución y si tus síntomas empeoran, sobre todo si tienes dificultad respiratoria, ponte en contacto con el teléfono designado por tu Comunidad para la atención sobre el COVID-19 o con tu centro de salud.</li>
+          </ul>
         </div>
         </Fragment>
       );
     } else {
       return(
         <Fragment>
+        <div className="mensaje">
+        <p>Tus respuestas <b>NO</b> sugieren la posibilidad de tener sintomatología<br></br> compatible con COVID-19</p>
         <input type="radio" className="radio" name="progress" value="twentyfive" id="bajo" defaultChecked></input>
         <div className="progress">
             <div className="progress-bar"></div>
+        </div>
         </div>
         </Fragment>
       );
--- src/components/Result.js
+++ src/components/Result.js
@@ -5,15 +5,13 @@ import Alertbar from "./Alertbar";
 const Result = ({score, playAgain, publicarSintomas, showMap}) => (
   <div className="score-board">
     <div className="score">
-      <p>Tu puntaje es: <b>{score}</b></p>
+      {/*<p>Tu puntaje es: <b>{score}</b></p>*/}
       <Alertbar score={score}/>
       <div className="mapa" id="mapa">
-      {showMap()}
+      {/*showMap()*/}
       </div>
-      <button className="publicBtn" onClick={publicarSintomas}>Publicar</button>
-      <button className="playBtn" onClick={playAgain}>
-      Otra vez
-    </button>
+      <button className="publicBtn" onClick={publicarSintomas}>PUBLICAR</button>
+      <button className="playBtn" onClick={playAgain}>OTRA VEZ</button>
     </div>
   </div>
 );
